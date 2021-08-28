0f320a44b40f1a0958b8c89a5644a6e3a5e6741c
Olgabrezel <lumpes8524@gmail.com>
2021-08-28 12:22:44 +0200

    Change WhoAmIBot from library to executable

---
+++ WhoAmIBot/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Threading;
+
+namespace WhoAmIBotSpace
+{
+    public class Program
+    {
+        private static WhoAmIBot Bot;
+
+        public static void Main(string[] args)
+        {
+            string token;
+
+            if (args.Length == 0)
+            {
+                Console.Write("Please enter the token (or pass it as console argument): ");
+                token = Console.ReadLine();
+            }
+            else
+            {
+                token = args[0];
+            }
+
+            Console.WriteLine("Starting WhoAmIBot...");
+
+            Bot = new WhoAmIBot(token);
+            Bot.StartBot();
+
+            Console.WriteLine("WhoAmIBot running!");
+
+            Thread.Sleep(-1);
+        }
+    }
+}
